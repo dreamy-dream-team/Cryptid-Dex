becef8e4a85ae3ead2828af59bf2c2d128bdc212
Rob <robertbalchii@gmail.com>
2023-11-09 14:11:36 -0700

    update to npm modules and fixed error code from initial create-next-app@latest command

--- cryptidex/src/pages/_document.ts
+++ cryptidex/src/pages/_document.ts
@@ -1,10 +1,7 @@
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-      </body>
-    </Html>
+        <Main> </Main>
+        </Html>
   );
 }
