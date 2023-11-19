6bb6a50109bafbb17309e153dd7ee826f43dff38
Rob <robertbalchii@gmail.com>
2023-11-18 17:39:14 -0700

    update directory structure and created basic navbars

--- cryptidex/src/pages/index.tsx
+++ cryptidex/src/pages/index.tsx
@@ -1,7 +1,15 @@
 import { Inter } from "next/font/google";
+import Navbar from "@/components/dashboard/Navbar";
+import Footer from "@/components/dashboard/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return null;
+  return (
+    <main>
+      <Navbar />
+
+      <Footer />
+    </main>
+  );
 }
